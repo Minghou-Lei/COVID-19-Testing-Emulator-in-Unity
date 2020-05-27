ad37de39ba96f0b2279275632100dc2bd8ee38c3
Minghou Lei <36237320+Mistletoer@users.noreply.github.com>
2020-05-27 10:44:12 +0800

    first commit

---
+++ Assets/Script/LiquidControl.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LiquidControl : MonoBehaviour
+{
+    public float fill;
+    private float value;
+    public Color color;
+    private Renderer liquid;
+    // Start is called before the first frame update
+    void Start()
+    {
+        liquid = transform.GetChild(0).GetComponent<Renderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Range(0.382,0.63) 
+        //  .382 full .63 empty
+        fill = Mathf.Clamp01(fill);
+        value = fill*-0.248f + 0.63f;
+        liquid.material.SetFloat("_FillAmount",value);
+        color.a=1f;
+        liquid.material.SetColor("_Tint",color);
+    }
+}
---
+++ Assets/Script/SampleExtrace.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using VRTK;
+
+public class SampleExtrace : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    public GameObject tubePrefab;
+    public float offset;
+    public VRTK_InteractableObject linkedObject;
+
+    protected virtual void OnEnable() {
+
+        if(linkedObject!=null){
+            linkedObject.InteractableObjectUsed += InteractableObjectUsed;
+        }
+
+    }
+
+    /// <summary>
+    /// This function is called when the behaviour becomes disabled or inactive.
+    /// </summary>
+    void OnDisable()
+    {
+        if (linkedObject != null)
+            {
+                linkedObject.InteractableObjectUsed -= InteractableObjectUsed;
+            }
+    }
+
+    protected virtual void InteractableObjectUsed(object sender, InteractableObjectEventArgs e)
+    {
+        GameObject tube = Instantiate(tubePrefab,new Vector3(transform.position.x,transform.position.y+offset,transform.position.z),Quaternion.Euler(0,0,0));
+        tube.GetComponent<LiquidControl>().fill = 0.5f;
+        tube.GetComponent<LiquidControl>().color = Color.blue;
+    }
+
+    void Start()
+    {
+        linkedObject = GetComponent<VRTK_InteractableObject>();
+    }
+}
